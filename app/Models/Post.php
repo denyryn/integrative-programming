9e944cf6e3116fc6d0009d10cc745581de3c40c2
Deny Rian <denyryn@outlook.com>
2026-04-19 18:01:19 +0700

    added missing relationships

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $fillable = ['title', 'status', 'content', 'user_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
