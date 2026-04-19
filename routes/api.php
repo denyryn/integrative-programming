163dccad0a0f7b01206ba12068981b62d7fee02f
Deny Rian <denyryn@outlook.com>
2026-04-19 18:01:03 +0700

    protect resource endpoint with auth middleware

--- routes/api.php
+++ routes/api.php
@@ -7,4 +7,9 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::resource('posts', App\Http\Controllers\PostController::class);
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::resource('posts', App\Http\Controllers\PostController::class)
+        ->only(['index', 'store', 'show', 'update', 'destroy']);
+    Route::resource('comments', App\Http\Controllers\CommentController::class)
+        ->only(['index', 'store', 'show', 'update', 'destroy']);
+});
