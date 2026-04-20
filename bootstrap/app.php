9f12250ad37233d0912b21a44ecdc6adf7b54946
Deny Rian <denyryn@outlook.com>
2026-04-21 00:52:29 +0700

    format error response and reorder by generality

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,35 +18,58 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions): void {
 
-        $exceptions->render(function (Symfony\Component\HttpKernel\Exception\NotFoundHttpException $e, $request) {
+        $exceptions->render(function (Illuminate\Database\Eloquent\ModelNotFoundException $e, $request) {
             return response()->json([
-                'message' => 'Route not found',
-            ], 404);
-        });
-
-        $exceptions->render(function (ModelNotFoundException $e, $request) {
-            return response()->json([
+                'success' => false,
                 'message' => 'Resource not found',
+                'data' => null,
+                'errors' => null,
             ], 404);
         });
 
         $exceptions->render(function (Illuminate\Auth\Access\AuthorizationException $e, $request) {
             return response()->json([
-                'message' => 'Forbidden'
+                'success' => false,
+                'message' => 'Forbidden',
+                'data' => null,
+                'errors' => null,
             ], 403);
         });
 
         $exceptions->render(function (Illuminate\Validation\ValidationException $e, $request) {
             return response()->json([
+                'success' => false,
                 'message' => 'Validation failed',
+                'data' => null,
                 'errors' => $e->errors(),
             ], 422);
         });
 
-        $exceptions->render(function (\Illuminate\Auth\AuthenticationException $e, $request) {
+        $exceptions->render(function (Illuminate\Auth\AuthenticationException $e, $request) {
             return response()->json([
-                'message' => 'Unauthenticated'
+                'success' => false,
+                'message' => 'Unauthenticated',
+                'data' => null,
+                'errors' => null,
             ], 401);
+        });
+
+        $exceptions->render(function (Symfony\Component\HttpKernel\Exception\NotFoundHttpException $e, $request) {
+            return response()->json([
+                'success' => false,
+                'message' => $e->getMessage(),
+                'data' => null,
+                'errors' => null,
+            ], 404);
+        });
+
+        $exceptions->render(function (Symfony\Component\HttpKernel\Exception\HttpException $e, $request) {
+            return response()->json([
+                'success' => false,
+                'message' => $e->getMessage(),
+                'data' => null,
+                'errors' => null,
+            ], 404);
         });
 
     })->create();
